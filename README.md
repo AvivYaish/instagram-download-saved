fac96a44dac9d32cf39fb76b5e9309a30df7cd2a
Aviv Yaish <aviv.yaish@mail.huji.ac.il>
2021-12-19 11:09:15 +0200

    Made the script runnable.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # instagram-download-saved
-Downloads photos you saved from a specific profile.
+A python script that downloads photos you saved from a specific profile.
+
+The only dependency is [instaloader](https://instaloader.github.io/), an open-source python package for scraping instagram.
+
+Usage:
+
+    python script.py <username-to-login-with> <username-to-download>
+    
+Afterwards, you will be prompted by instaloader to enter your password.
+
+According to instaloader's [code](https://github.com/instaloader/instaloader), the password is not saved anywhere.
--- instagram_download_saved/script.py
+++ instagram_download_saved/script.py
@@ -1,14 +1,19 @@
+import sys
 import pathlib
 import instaloader
 
-username_login: str         = "<enter-your-login-username-here>"
-username_to_download: str   = "<enter-the-username-to-download-here>"
+if "__main__" == __name__:
+    if not len(sys.argv) == 3:
+        sys.exit("Invalid usage, please use: python script.py <username-to-login-with> <username-to-download>")
 
-L = instaloader.Instaloader()
-L.interactive_login(username_login)     # This will ask for your password
-profile = str(instaloader.Profile.from_username(L.context, username_to_download).userid)
-download_path = pathlib.Path('.')
+    username_login: str         = sys.argv[1]
+    username_to_download: str   = sys.argv[2]
 
-for post in instaloader.Profile.from_username(L.context, username_login).get_saved_posts():
-    if post.owner_id == profile:
-        L.download_pic(str(download_path.joinpath(pathlib.Path(str(int(post.date.timestamp()))))), url=post.url, mtime=post.date)
+    L = instaloader.Instaloader()
+    L.interactive_login(username_login)     # This will ask for your password
+    profile = str(instaloader.Profile.from_username(L.context, username_to_download).userid)
+    download_path = pathlib.Path('.')
+
+    for post in instaloader.Profile.from_username(L.context, username_login).get_saved_posts():
+        if post.owner_id == profile:
+            L.download_pic(str(download_path.joinpath(pathlib.Path(str(int(post.date.timestamp()))))), url=post.url, mtime=post.date)
